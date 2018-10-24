632302ca5b508dfad8645a0c1f89f2f962013337
dajfactor <dajfactor@gmail.com>
2018-10-24 01:03:54 -0700

    Added command binding for adding/losing health

--- BattleconCompanion/BattleconCompanion/Models/GameState.cs
+++ BattleconCompanion/BattleconCompanion/Models/GameState.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace BattleconCompanion.Models
 {
@@ -15,6 +17,42 @@ namespace BattleconCompanion.Models
             GameLength = 15;
 
             Players = new List<Player>();
+
+            AdvanceToNextBeat = new Command(
+                execute: () =>
+                {
+                    CurrentBeat++;
+                    foreach(var player in Players)
+                    {
+                        var forceAmount = 1;
+
+                        if(player.LifeTotal <= 7)
+                        {
+                            forceAmount++;
+                        }
+
+                        ForceRemaining -= forceAmount;
+                        player.CurrentForce += forceAmount;
+
+                        //Maximum 10 force
+                        player.CurrentForce = player.CurrentForce > 10 ? 10 : player.CurrentForce;
+                    }
+                }
+            );
+
+            //Refactor these later, this is gross
+            DamagePlayer = new Command<string>(
+                execute: (string playerNumber) =>
+                {
+                    Players[int.Parse(playerNumber)].LifeTotal--;
+                });
+
+            HealPlayer = new Command<string>(
+                execute: (string playerNumber) =>
+                {
+                    Players[int.Parse(playerNumber)].LifeTotal++;
+                    Players[int.Parse(playerNumber)].LifeTotal = Players[int.Parse(playerNumber)].LifeTotal > 20 ? 20 : Players[int.Parse(playerNumber)].LifeTotal;
+                });
         }
 
         public void AddPlayer()
@@ -26,7 +64,6 @@ namespace BattleconCompanion.Models
         {
             Players.RemoveAt(playerNumber);
         }
-
 
         int _forceRemaining;
         int _currentBeat;
@@ -53,6 +90,11 @@ namespace BattleconCompanion.Models
 
         public List<Player> Players { get; set; }
         public int GameLength { get; set; }
+
+        public ICommand AdvanceToNextBeat { private set; get; }
+        public ICommand UpdateForce { private set; get; }
+        public ICommand DamagePlayer { private set; get; }
+        public ICommand HealPlayer { private set; get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         void OnPropertyChanged([CallerMemberName] string propertyName = null)
