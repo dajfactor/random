475b0f34c8ee7dc2757a4e6a4217ef6e70113613
dajfactor <dajfactor@gmail.com>
2018-10-24 01:32:33 -0700

    Added a test CSS sheet. Added a custom? label HTML renderer, but might not need

---
+++ BattleconCompanion/BattleconCompanion.Android/HtmlFormatLabelRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.Text;
+using Android.Widget;
+using BattleconCompanion.Droid;
+using BattleconCompanion.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(HtmlFormatLabel), typeof(HtmlFormatLabelRenderer))]
+namespace BattleconCompanion.Droid
+{
+    public class HtmlFormatLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var view = (HtmlFormatLabel)Element;
+            if (view == null) return;
+
+            Control.SetText(Html.FromHtml(view.Text.ToString(), FromHtmlOptions.ModeLegacy), TextView.BufferType.Spannable);
+        }
+    }
+}
---
+++ BattleconCompanion/BattleconCompanion/Helpers/HtmlFormatLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BattleconCompanion.Helpers
+{
+    public class HtmlFormatLabel : Label
+    {
+
+    }
+}
+
